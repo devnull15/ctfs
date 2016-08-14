3f0cff8bf7ce950943f75054b5dcf2fe8ebcdf89
devnull15 <dev@comcast.net>
2016-08-14 15:53:10 -0400

    working hill...

---
+++ icectf/hill/cipher2num.py
@@ -0,0 +1,6 @@
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789_{}"
+
+ciphertext = "7Nv7}dI9hD9qGmP}CR_5wJDdkj4CKxd45rko1cj51DpHPnNDb__EXDotSRCP8ZCQ"
+
+for c in ciphertext:
+    print alphabet.find(c)
